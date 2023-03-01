603ba7d0d9ed97ceae31fd116b5d2630b6d2fe56
Shandra Morton <shanmortonbootcamp@gmail.com>
2023-03-01 19:35:50 +0000

    updated app.py with x-ray config

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,6 +36,14 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 #HoneyComb - end
 
+#Xray
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+#end Xray
 
 app = Flask(__name__)
 #HoneyComb
