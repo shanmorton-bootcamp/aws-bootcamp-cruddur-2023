f3930ea48a3cb96e2467ff80f4800a55d76b8052
Shandra Morton <shanmortonbootcamp@gmail.com>
2023-03-01 20:13:31 +0000

    moved xray middleware line

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -45,10 +45,11 @@ tracer = trace.get_tracer(__name__)
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 #end Xray
 
 app = Flask(__name__)
+XRayMiddleware(app, xray_recorder)
 #HoneyComb
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
