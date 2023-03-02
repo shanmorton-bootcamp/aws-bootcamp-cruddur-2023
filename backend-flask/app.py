a028e541b886750f7657ce11b704fe4b2a8e7a90
Shandra Morton <shanmortonbootcamp@gmail.com>
2023-03-02 20:24:13 +0000

    a line i missed from the instructions, maybe

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -42,9 +42,9 @@ tracer = trace.get_tracer(__name__)
 
 #Xray
 
-
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)ynamic_naming=xray_url)
 
 #end Xray
 
